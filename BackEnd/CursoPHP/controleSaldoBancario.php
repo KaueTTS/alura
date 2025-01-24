89a82f7b878d6556f22efdf3bae4baaf428fc50f
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2025-01-24 05:54:29 -0300

    Curso PHP

---
+++ BackEnd/CursoPHP/controleSaldoBancario.php
@@ -0,0 +1,12 @@
+<?php
+
+$conta_bancaria = [
+    "titular" => "Kauê Bertaze",
+    "saldo" => 1000
+];
+
+
+echo "***************" . PHP_EOL;
+echo "Titular: " . $conta_bancaria["titular"] . PHP_EOL;
+echo "Saldo Atual: " . $conta_bancaria["saldo"] . PHP_EOL;
+echo "***************" . PHP_EOL;
