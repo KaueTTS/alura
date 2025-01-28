52d2efd5d4e7c6da1294b502608a4ecd1e3ab4a8
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2025-01-28 17:41:07 -0300

    Curso php

--- BackEnd/CursoPHP/Evoluindo o ScreenMatch/calculoImc.php
+++ BackEnd/CursoPHP/Evoluindo o ScreenMatch/calculoImc.php
@@ -4,3 +4,5 @@ function calculaImc(float $alturaEmMetros, float $pesoEmQuilos): float
 {
     return $pesoEmQuilos / $alturaEmMetros ** 2;
 }
+
+echo calculaImc(1.70, 70);
--- BackEnd/CursoPHP/Evoluindo o ScreenMatch/celsiusParaFahrenheit.php
+++ BackEnd/CursoPHP/Evoluindo o ScreenMatch/celsiusParaFahrenheit.php
@@ -1,6 +1,8 @@
 <?php
 
-function celsiusToFahrenheit(float$celsius): float
+function celsiusToFahrenheit(float $celsius): float
 {
     return ($celsius * 9 / 5) + 32;
 }
+
+echo celsiusToFahrenheit(100); // 212
