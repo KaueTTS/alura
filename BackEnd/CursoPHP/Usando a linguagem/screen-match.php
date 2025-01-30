d828baf684ceeadabe4692c24078f964904d60f6
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2025-01-30 08:21:56 -0300

    Curso PHP

--- BackEnd/CursoPHP/Usando a linguagem/screen-match.php
+++ BackEnd/CursoPHP/Usando a linguagem/screen-match.php
@@ -16,12 +16,16 @@ $notaFilme =  array_sum($notas) / $quantidadeDeNotas;
 
 
 
-var_dump($notas);
-sort($notas);
-var_dump($notas);
-$menorNota = min($notas);
-echo $menorNota;
+// var_dump($notas);
+// sort($notas);
+// var_dump($notas);
+// $menorNota = min($notas);
+// echo $menorNota;
 
 
-var_dump($filme['nome']);
-var_dump(strpos($filme['nome'], "c"));
+// var_dump($filme['nome']);
+// $posicaoDoisPontos = strpos($filme['nome'], "c");
+// var_dump($posicaoDoisPontos);
+
+
+var_dump(substr($filme['nome'], 0, 3, $posicaoDoisPontos));
