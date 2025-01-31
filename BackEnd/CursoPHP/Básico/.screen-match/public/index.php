d24277b2cfeb03e242bdac670146c03e6e3a5f41
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2025-01-31 19:37:39 -0300

    Curso PHP

--- BackEnd/CursoPHP/Básico/.screen-match/public/index.php
+++ BackEnd/CursoPHP/Básico/.screen-match/public/index.php
@@ -9,35 +9,41 @@
 </head>
 
 <body>
-    <nav></nav>
-    <h1>Exportador do ScreenMatch</h1>
-    <form action="exporta-arquivo.php" method="post">
-        <fieldset>
-            <label for="nome">Nome:</label> <br>
-            <input type="text" name="nome" id="nome" required>
-        </fieldset>
+    <nav class="navbar">
+        <h1>Exportador do ScreenMatch</h1>
+        <a href="./">Voltar</a>
+    </nav>
 
-        <fieldset>
-            <label for="ano">Ano de lançamento:</label> <br>
-            <input type="year" name="ano" id="ano" required>
-        </fieldset>
+    <section class="container">
+        <form action="exporta-arquivo.php" method="post">
+            <fieldset>
+                <label for="nome">Nome:</label> <br>
+                <input type="text" name="nome" id="nome" required>
+            </fieldset>
 
-        <fieldset>
-            <label for="nome">Nota:</label> <br>
-            <input type="number" name="nota" id="nota" required step="0.1">
-        </fieldset>
+            <fieldset>
+                <label for="ano">Ano de lançamento:</label> <br>
+                <input type="year" name="ano" id="ano" required>
+            </fieldset>
 
-        <fieldset>
-            <label for="nome">Gênero:</label> <br>
-            <select name="genero" id="genero">
-                <option value="super-heroi">Super-herói</option>
-                <option value="comedia">Comédia</option>
-                <option value="acao">Ação</option>
-            </select>
-        </fieldset>
+            <fieldset>
+                <label for="nome">Nota:</label> <br>
+                <input type="number" name="nota" id="nota" required step="0.1">
+            </fieldset>
 
-        <br>
-        <input type="submit" value="Enviar">
-    </form>
+            <fieldset>
+                <label for="nome">Gênero:</label> <br>
+
+                <select name="genero" id="genero">
+                    <option value="super-heroi">Super-herói</option>
+                    <option value="comedia">Comédia</option>
+                    <option value="acao">Ação</option>
+                    <option value="acao">Terror</option>
+                </select>
+            </fieldset>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
 </body>
 </html>
