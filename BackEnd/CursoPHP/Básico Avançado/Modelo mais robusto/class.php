2600168c6a484dc11444a3eac7931cdd2f617409
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2025-02-05 21:31:21 -0300

    Curso PHP

--- BackEnd/CursoPHP/Básico Avançado/Modelo mais robusto/class.php
+++ BackEnd/CursoPHP/Básico Avançado/Modelo mais robusto/class.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'enum.php';
+
 class Conta
 {
     private int $saldoEmCentavos;
@@ -11,17 +13,33 @@ class Conta
         $this->saldoEmCentavos = 0;
     }
 
-    public function depositar(int $valorADepositar): void
+    public function depositar(int $valorADepositar): string
     {
         if ($valorADepositar > 0) {
             $this->saldoEmCentavos += $valorADepositar;
+            return "\n\nValor depositado: {$valorADepositar} centavos";
         }
     }
 
-    public function sacar(int $valorASacar): void
+    public function sacar(int $valorASacar): string
     {
         if ($valorASacar > 0 && $valorASacar < $this->saldoEmCentavos) {
             $this->saldoEmCentavos -= $valorASacar;
+            return "\n\nValor sacado: {$valorASacar} centavos";
+        } else {
+            return "\n\nSaldo insuficiente para saque ou valor inválido";
         }
     }
+
+    public function getSaldo(): string
+    {
+        return "\n\nSaldo atual no banco: {$this->saldoEmCentavos} centavos";
+    }
 }
+
+$conta = new Conta('João', TipoConta::Poupanca);
+echo $conta->getSaldo();
+echo $conta->depositar(100);
+echo $conta->getSaldo();
+echo $conta->sacar(50);
+echo $conta->getSaldo();
